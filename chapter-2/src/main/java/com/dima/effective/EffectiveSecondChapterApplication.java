77aaeb99fea6efca2523be8142d7ab7d7435e84a
rozhko.dmytro <dimarozhko2000@gmail.com>
2021-09-16 03:39:02 +0300

    [ITEM-2] Builder

--- chapter-2/src/main/java/com/dima/effective/EffectiveSecondChapterApplication.java
+++ chapter-2/src/main/java/com/dima/effective/EffectiveSecondChapterApplication.java
@@ -1,6 +1,7 @@
 package com.dima.effective;
 
 import com.dima.effective.item1.Primitive;
+import com.dima.effective.item2.builder.NutritionFacts;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -14,6 +15,16 @@ public class EffectiveSecondChapterApplication {
         }
         else {
             log.error("Primitive.valueOf incorrect");
+        }
+        NutritionFacts nutritionFacts = new NutritionFacts.Builder(12, 33).setFat(189).setCalories(659).setCalories(6).build();
+        log.info("Item 2");
+        if (nutritionFacts.getServingSize() == 12 && nutritionFacts.getServings() == 33
+                && nutritionFacts.getFat() == 189 && nutritionFacts.getCalories() == 6
+                && nutritionFacts.getCarbohydrate() == 0 && nutritionFacts.getSodium() == 0) {
+            log.info("NutritionFacts.Builder correct");
+        }
+        else {
+            log.error("NutritionFacts.Builder incorrect");
         }
     }
 }
---
+++ chapter-2/src/main/java/com/dima/effective/item2/builder/NutritionFacts.java
@@ -0,0 +1,83 @@
+package com.dima.effective.item2.builder;
+
+public class NutritionFacts {
+
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder {
+        private final int servingSize;
+        private final int servings;
+        // optional
+        private int calories;
+        private int fat;
+        private int sodium;
+        private int carbohydrate;
+
+        public Builder (int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder setCalories(int value) {
+            this.calories = value;
+            return this;
+        }
+
+        public Builder setFat(int value) {
+            this.fat = value;
+            return this;
+        }
+
+        public Builder setSodium(int value) {
+            this.sodium = value;
+            return this;
+        }
+
+        public Builder setCarbohydrate(int value) {
+            this.carbohydrate = value;
+            return this;
+        }
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    public NutritionFacts(Builder builder) {
+        this.servingSize = builder.servingSize;
+        this.servings = builder.servings;
+        this.calories = builder.calories;
+        this.fat = builder.fat;
+        this.sodium = builder.sodium;
+        this.carbohydrate = builder.carbohydrate;
+    }
+
+    public int getServingSize() {
+        return servingSize;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public int getFat() {
+        return fat;
+    }
+
+    public int getSodium() {
+        return sodium;
+    }
+
+    public int getCarbohydrate() {
+        return carbohydrate;
+    }
+}
