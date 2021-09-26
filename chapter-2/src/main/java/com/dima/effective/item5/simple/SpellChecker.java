91c3d298719c3f6160dc1a2e96e54b9e90f50336
rozhko.dmytro <dimarozhko2000@gmail.com>
2021-09-27 02:59:41 +0300

    ITEM5

--- chapter-2/src/main/java/com/dima/effective/item5/simple/SpellChecker.java
+++ chapter-2/src/main/java/com/dima/effective/item5/simple/SpellChecker.java
@@ -1,5 +1,7 @@
 package com.dima.effective.item5.simple;
 
+import com.dima.effective.item5.utility.UtilityLexicon;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -9,23 +11,6 @@ import java.util.stream.Collectors;
 public class SpellChecker {
 
     private static final Map<String, String> dictionary = Collections.unmodifiableMap(UtilityLexicon.dictionaryCreator());
-
-    private static class UtilityLexicon {
-
-        private static Map<String, String> dictionary;
-
-        private UtilityLexicon () {
-            throw new AssertionError();
-        }
-
-        public static Map<String, String> dictionaryCreator() {
-            UtilityLexicon.dictionary = new HashMap<>();
-            UtilityLexicon.dictionary.put("fire", "valid");
-            UtilityLexicon.dictionary.put("get", "not");
-            return UtilityLexicon.dictionary;
-        }
-
-    }
 
     private SpellChecker() {}
 
