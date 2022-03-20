9a0a971976372a3ed6d837afa909589bfb762bb1
rozhko.dmytro <dimarozhko2000@gmail.com>
2022-03-20 19:43:04 +0200

    CHAPTER3 Initialisation

---
+++ chapter-3/src/main/java/com/dima/effective/EffectiveThirdChapterApplication.java
@@ -0,0 +1,11 @@
+package com.dima.effective;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class EffectiveThirdChapterApplication {
+
+    public static void main(String[] args) {
+        log.info("CHAPTER 3");
+    }
+}
